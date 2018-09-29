4fbbd55d0df8eb6572c500fe2f99ab65b4bbd417
Artem Orynko <a.orynko@gmail.com>
2018-09-29 08:14:31 +0300

    Initial Commit

---
+++ Sources/Glogger.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Glogger_iOS.
+FOUNDATION_EXPORT double GloggerVersionNumber;
+
+//! Project version string for Glogger_iOS.
+FOUNDATION_EXPORT const unsigned char GloggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Glogger_iOS/PublicHeader.h>
+
+
