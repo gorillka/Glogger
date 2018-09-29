b7cd0efe129fb8a734e373fa099ff76e33055b96
Artem Orynko <a.orynko@gmail.com>
2018-09-29 08:45:00 +0300

    add: `LogLevel`

---
+++ Sources/LogLevel.swift
@@ -0,0 +1,16 @@
+
+import os.log
+
+public enum LogLevel: Int {
+    case verbose = 0, info, debug, warning, error
+
+    internal var osLogType: OSLogType {
+        switch self {
+        case .verbose: return .default
+        case .info: return .info
+        case .debug: return .debug
+        case .warning: return .error
+        case .error: return .fault
+        }
+    }
+}
